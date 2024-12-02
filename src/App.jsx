0123999ba0d731fe5d6a2b88da2e16cabc8c2e3d
ShahedMiah <47751858+ShahedMiah@users.noreply.github.com>
2024-12-02 20:53:45 +0000

    Add App.jsx as main component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GameContainer from './components/game/GameContainer';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <GameContainer />
+    </div>
+  );
+};
+
+export default App;
